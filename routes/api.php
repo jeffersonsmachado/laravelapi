f205c78cdcc5a554450320415ce0b43be83bbc1b
Jefferson Machado <jeffersonsmachado@gmail.com>
2022-11-13 16:42:43 -0300

    Api Authentication

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::post('/tokens/create', function (Request $request) {
+//     $token = $request->user()->createToken($request->token_name);
+ 
+//     return ['token' => $token->plainTextToken];
+// });
+
+Route::group([
+    'prefix'     => 'v1',
+    'as'         => 'api.',
+    // 'namespace'  => 'Api\V1\Admin',
+    // 'middleware' => ['auth:sanctum']
+], function() {
+    // Route::apiResource('expense', ExpenseController::class)->withTrashed();
+    Route::post('login', 'App\\Http\\Controllers\\Api\\AuthController@login');
 });
